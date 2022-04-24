47a10fcc56cdfd6370ed1928c2bdc095a53d204c
jakaria_ridoy <jh_ridoy@yahoo.com>
2022-04-24 15:49:53 +0600

    first commit

---
+++ src/test/java/com/jakaria/demojenkinsdocker/DemoJenkinsDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakaria.demojenkinsdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJenkinsDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
