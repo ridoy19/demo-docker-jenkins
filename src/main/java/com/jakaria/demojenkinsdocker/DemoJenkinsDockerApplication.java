3c78d99bcb6c344e5b6ff6c0c6ba0dbffbf5a381
jakaria_ridoy <jh_ridoy@yahoo.com>
2022-04-25 11:07:37 +0600

    Added path variable

--- src/main/java/com/jakaria/demojenkinsdocker/DemoJenkinsDockerApplication.java
+++ src/main/java/com/jakaria/demojenkinsdocker/DemoJenkinsDockerApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -16,7 +17,7 @@ public class DemoJenkinsDockerApplication {
 	}
 
 	@GetMapping
-	public ResponseEntity<String> sayHello() {
-		return new ResponseEntity<>("Hello, Jakaria", HttpStatus.OK);
+	public ResponseEntity<String> sayHello(@PathVariable String name) {
+		return new ResponseEntity<>("Hello, " + name, HttpStatus.OK);
 	}
 }
