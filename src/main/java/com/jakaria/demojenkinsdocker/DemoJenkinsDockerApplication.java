8d9c41683ab829fa58a8d8e4b08926c9c7367011
jakaria_ridoy <jh_ridoy@yahoo.com>
2022-04-25 11:17:50 +0600

    Added Requestparam

--- src/main/java/com/jakaria/demojenkinsdocker/DemoJenkinsDockerApplication.java
+++ src/main/java/com/jakaria/demojenkinsdocker/DemoJenkinsDockerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -17,7 +18,7 @@ public class DemoJenkinsDockerApplication {
 	}
 
 	@GetMapping
-	public ResponseEntity<String> sayHello(@PathVariable String name) {
+	public ResponseEntity<String> sayHello(@RequestParam String name) {
 		return new ResponseEntity<>("Hello, " + name, HttpStatus.OK);
 	}
 }
